ff44c68737b2d262288a08d01c6ccc53cd1e57ba
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2018-01-29 14:51:29 -0500

    Moved ReadME and setup Header.js

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, Collapse, NavbarToggler } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+
+      isOpen: false,
+      isAuthenticated: true
+    };
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  handleLogout() {
+    this.setState({
+      isAuthenticated: false
+    });
+  }
+
+  render() {
+    return (    
+      <div className="container-width">
+        <Navbar color="#297CBB" expand="md">
+          <NavbarBrand href="/"><img height="35%"  width="35%" src="../../public/favicon.ico" alt="logo" /></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar >
+          <Nav className="ml-auto" navbar>
+            <NavItem style={{ visibility: this.state.isAuthenticated ? 'visible' : 'hidden' }}>
+              <NavLink activeClassName="active" to="/dashboard">Home</NavLink>
+            </NavItem>
+            <NavItem style={{ visibility: this.state.isAuthenticated ? 'visible' : 'hidden' }}>
+              <NavLink activeClassName="active" onClick={this.handleLogout} to="/logout">Logout</NavLink>
+            </NavItem>
+            <NavItem style={{ visibility: this.state.isAuthenticated ? 'hidden' : 'visible' }}>
+              <NavLink activeClassName="active" to="/">Login</NavLink>
+            </NavItem>
+            <NavItem style={{ visibility: this.state.isAuthenticated ? 'hidden' : 'visible' }}>
+              <NavLink activeClassName="active" to="/register">Register</NavLink>
+            </NavItem>
+          </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )}
+}
+export default Header;
